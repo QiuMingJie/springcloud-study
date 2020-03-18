85439769c9ba4f34ae88f8255616f70595bd10ad
qiumingjie <838689269@qq.com>
2020-03-18 16:19:40 +0800

    新建两个消费者

---
+++ springcloud-client-order/src/main/java/com/qiumingjie/api/controller/OrderController.java
@@ -0,0 +1,26 @@
+package com.qiumingjie.api.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+/**
+ * @author QiuMingJie
+ * @date 2020-03-17 17:19
+ * @description
+ */
+@RestController
+public class OrderController {
+
+    @Autowired
+    private RestTemplate restTemplate;
+
+    @RequestMapping(value = "/getOrder",method = RequestMethod.GET)
+    public String getNumber() {
+        String forObject = restTemplate.getForObject("http://localhost:8000/getMember", String.class);
+        System.out.println(forObject);
+        return "";
+    }
+}
