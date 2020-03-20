c2effd87cd0422a511afb4104b39b7c666afbe7a
qiumingjie <838689269@qq.com>
2020-03-20 18:35:32 +0800

    添加zookeeper

---
+++ springcloud-service-zookeeper/src/main/java/com/qiumingjie/springcloudserverzookeeper/SpringCloudServerZookeeper.java
@@ -0,0 +1,15 @@
+package com.qiumingjie.springcloudserverzookeeper;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+@SpringBootApplication
+@EnableDiscoveryClient
+public class SpringCloudServerZookeeper {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringCloudServerZookeeper.class, args);
+    }
+
+}
