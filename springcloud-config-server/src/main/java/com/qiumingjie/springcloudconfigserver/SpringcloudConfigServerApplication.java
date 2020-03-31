00c29a923689b131cac6347814d356a2bd09a64e
qiumingjie <838689269@qq.com>
2020-03-31 15:11:54 +0800

    尝试配置springcloudconfig

--- springcloud-config-server/src/main/java/com/qiumingjie/springcloudconfigserver/SpringcloudConfigServerApplication.java
+++ springcloud-config-server/src/main/java/com/qiumingjie/springcloudconfigserver/SpringcloudConfigServerApplication.java
@@ -2,8 +2,12 @@ package com.qiumingjie.springcloudconfigserver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
+@EnableEurekaClient
+@EnableConfigServer
 public class SpringcloudConfigServerApplication {
 
     public static void main(String[] args) {
